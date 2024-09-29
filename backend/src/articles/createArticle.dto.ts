2136288f5ce10ac9ed7ad57f1ecfac7d93717ac5
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2024-09-29 17:05:16 +1300

    Creating interface for creating Articles

---
+++ backend/src/articles/createArticle.dto.ts
@@ -0,0 +1,9 @@
+export interface CreateArticleDTO {
+  title: string;
+  authors: string[];
+  source: string;
+  pubyear: number;
+  doi: string;
+  summary: string;
+  linked_discussion: string;
+}
