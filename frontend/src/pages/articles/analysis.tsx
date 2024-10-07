0930a1cc6d75f09b6c19d1c23686968c9ef9952e
lspain0 <lucasspain0@gmail.com>
2024-10-07 14:54:27 +1300

    Added page for list of articles in the analysis queue

---
+++ frontend/src/pages/articles/analysis.tsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react';
+import { ArticleInterface } from "@/utils/article.interface";
+
+const AnalysisQueuePage = () => {
+
+var current = 1
+
+  // API call function
+const fetchArticles = async () => {
+	try {
+		const response = await fetch("http://localhost:3000/api/articles/approved"); // fetch all approved articles
+		if (!response.ok) {
+			throw new Error("Failed to fetch");
+		}
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.error("Error fetching articles:", error);
+		return null; // Return null if there's an error
+	}
+};
+
+  const [articles, setArticles] = useState<ArticleInterface[]>([]);
+
+	useEffect(() => {
+		const getArticles = async () => {
+			const fetchedArticles = await fetchArticles();
+			if (fetchedArticles) {
+				setArticles(fetchedArticles);
+			}
+		};
+		getArticles();
+	}, []);
+
+  //list of articles that have been submitted
+  const queueList = articles.map((article) => (
+    <li key={(article as any)._id}>
+      <p><strong>Submission{' '+current++}</strong></p>
+      <p>
+        <strong>Title: </strong>{article.title}<br />
+        <strong>Authors: </strong> {article.authors}<br />
+        <strong>Source: </strong> {article.source}<br />
+        <strong>Publication Year: </strong> {article.pubyear}<br />
+        <strong>DOI: </strong> {article.doi}<br />
+        <strong>Claim: </strong> {article.claim}<br />
+        <strong>Evidence: </strong> {article.evidence}
+      </p>
+      <button>Analysis Form</button>
+      <br /><br />
+    </li>
+  ));
+
+  return (
+    <div className="container">
+      <h1>Analysis Queue</h1>
+      <ul>{queueList}</ul>
+    </div>
+  );
+};
+
+export default AnalysisQueuePage;
