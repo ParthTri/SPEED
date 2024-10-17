326b017a6f1915b26e60d996be68d38e1fb9ef73
lspain0 <lucasspain0@gmail.com>
2024-10-18 12:37:51 +1300

    Fixed incorrect API link

--- frontend/src/pages/articles/analysis.tsx
+++ frontend/src/pages/articles/analysis.tsx
@@ -45,7 +45,7 @@ const fetchArticles = async () => {
         <strong>Claim: </strong> {article.claim}<br />
         <strong>Evidence: </strong> {article.evidence}
       </p>
-      <button onClick={() => window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/articles/analysis/${(article as any)._id}`}>Analysis Form</button>
+      <button onClick={() => window.location.href = `/articles/analysis/${(article as any)._id}`}>Analysis Form</button>
       <br /><br />
     </li>
   ));
